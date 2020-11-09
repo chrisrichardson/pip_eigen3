081d9a867c78ad1507c5644c161fb0246db905d5
Chris Richardson <chris@bpi.cam.ac.uk>
2020-11-09 10:51:20 +0000

    Reorganise a bit

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from skbuild import setup
 
 setup(
-    name="eigen3",
+    name="eigen",
     version="3.3.8",
     description="Eigen3 C++ template library for linear algebra",
     author="Eigen3 team",
     license="MPL2",
+    package_dir={"eigen": "src"},
     packages=["eigen"],
 )
