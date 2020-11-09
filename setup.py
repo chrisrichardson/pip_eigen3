c8da66002c84badf6c22c9e8b23f7935018b37a6
Chris Richardson <chris@bpi.cam.ac.uk>
2020-11-09 10:36:57 +0000

    Install eigen with skbuild

---
+++ setup.py
@@ -0,0 +1,10 @@
+from skbuild import setup
+
+setup(
+    name="eigen3",
+    version="3.3.8",
+    description="Eigen3 C++ template library for linear algebra",
+    author="Eigen3 team",
+    license="MPL2",
+    packages=["eigen"],
+)
